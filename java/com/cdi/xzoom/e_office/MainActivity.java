7f5d7c3c4cf25896702d0dda0938130e6757a121
Not Even Kusuo <amanwhohave@gmail.com>
2021-03-26 10:46:44 +0700

    Update MainActivity.java

--- java/com/cdi/xzoom/e_office/MainActivity.java
+++ java/com/cdi/xzoom/e_office/MainActivity.java
@@ -4,31 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.CookieManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WebView myWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        WebView myWebView = (WebView) findViewById(R.id.mybrowser);
+        myWebView = (WebView) findViewById(R.id.mybrowser);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://officiacentre.000webhostapp.com/Login/login1.php");
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.getSettings().setBuiltInZoomControls(true);
+        myWebView.getSettings().setDisplayZoomControls(false);
+        myWebView.getSettings().setUseWideViewPort(true);
 
-                WebView view = new WebView(this);
-                view.getSettings().setJavaScriptEnabled(true);
-                view.setWebViewClient(new WebViewClient());
-                view.loadUrl("https://officiacentre.000webhostapp.com/Login/login1.php");
-                setContentView(view);
-                view.getSettings().setBuiltInZoomControls(true);
-                view.getSettings().setDisplayZoomControls(false);
-                myWebView.setWebViewClient(new WebViewClient());
+
+
 
         String cookies = CookieManager.getInstance().getCookie("https://officiacentre.000webhostapp.com");
         if (Build.VERSION.SDK_INT < 16) {
@@ -39,6 +37,14 @@ public class MainActivity extends AppCompatActivity {
             // Show Status Bar.
             int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE;
             decorView.setSystemUiVisibility(uiOptions);
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
         }
     }
 }
